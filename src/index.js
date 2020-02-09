379757247509ee411a667c38c8709093543a359a
Owen Sullivan <multiojuice@gmail.com>
2020-02-08 21:15:45 -0500

    Apis now working

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import '@babel/polyfill';
 import express from "express";
+
 import router from './handlers'
 import './mongo';
 
 const app = express();
-app.use(express.json)
+app.use(express.json());
 app.use('/', router);
 
 app.listen('3002', () => {console.warn('WE up!')});
